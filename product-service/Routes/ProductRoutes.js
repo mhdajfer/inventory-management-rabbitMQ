6adb47d3a44ec700d2cc763e64e1405a31e4c65a
muhammed <ajferaju9961@gmail.com>
2024-08-10 10:50:45 +0530

    product routes updated

--- product-service/Routes/ProductRoutes.js
+++ product-service/Routes/ProductRoutes.js
@@ -1,42 +1,10 @@
 const express = require("express");
 const ProductModel = require("../models/Product");
+const ProductController = require("../Controllers/ProductController");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const products = await ProductModel.find();
-    if (!products)
-      return res.status(200).json({ message: "No products available" });
+router.get("/", ProductController.getProducts);
 
-    return res.status(200).json({ products, message: "Products available" });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-    console.log(`server error while retrieving products: ${error.message}`);
-  }
-});
-
-router.post("/new", async (req, res) => {
-  try {
-    const { name, description, price } = req.body;
-
-    if (!name || !description || !price)
-      return res.status(400).json({ message: "Invalid parameters" });
-
-    const newProduct = new ProductModel({
-      name,
-      description,
-      price,
-    });
-
-    await newProduct.save();
-
-    return res
-      .status(201)
-      .json({ newProduct, message: "Product saved successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-    console.log(`server error while creating product: ${error.message}`);
-  }
-});
+router.post("/new", ProductController.createProduct);
 
 module.exports = router;
